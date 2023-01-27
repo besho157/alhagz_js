19d13b719f1c4600fdc805ae0ded723ca1a3d896
besho157 <beshomeshel15@gmail.com>
2023-01-27 13:08:35 +0200

    my code

--- main.js
+++ main.js
@@ -13,17 +13,4 @@ for (var i = 0; i < elements.length; i++) {
   };
 }
 
-var elements = document.getElementsByClassName("threecard");
-for (var i = 0; i < elements.length; i++) {
-  elements[i].onclick = function () {
-    var el = elements[0];
-    while (el) {
-      if (el.tagName === "DIV") {
-        el.classList.remove("active");
-      }
-      el = el.nextSibling;
-    }
 
-    this.classList.add("active");
-  };
-}
